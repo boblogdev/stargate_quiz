0339dc039694cf4433a8cc29efa7151fe3327eea
boblogdev <bobby.v.logan@googlemail.com>
2023-04-05 11:54:15 +0000

    add js update easy questions object like array

---
+++ assets/js/script.js
@@ -0,0 +1,89 @@
+// Create questions object like array with easyQuestions
+
+const easyQuestions = [
+    {
+      question: "What is the name of the alien race that originally built the Stargates in Stargate SG1?",
+      choices: ["The Ancients", "The Goa'uld", "The Tau'ri", "The Asgard"],
+      answer: 0
+    },
+    {
+      question: "What is the name of the primary protagonist in Stargate SG1?",
+      choices: ["Ba'al", "Colonel Jack O'Neill", "Jacob Carter", "Radek Zelenka"],
+      answer: 1
+    },
+    {
+      question: "What is the name of the planet where the SG1 team first meets the Goa'uld, Apophis?",
+      choices: ["Earth", "Chulak", "P3X-988", "Mars"],
+      answer: 1
+    },
+    {
+      question: "Who is the SG1 team's main nemesis throughout most of the series?",
+      choices: ["The Goa'uld", "The Ori", "The Jaffa", "Humanity"],
+      answer: 0
+    },
+    {
+      question: "What is the name of the advanced technology that SG1 acquires from the Asgard, allowing them to travel vast distances instantly?",
+      choices: ["Warp Drive", "Combustion", "Steam", "The Asgard Hyperdrive"],
+      answer: 4
+    },
+    {
+      question: "What is the name of the ancient Egyptian deity that the Goa'uld often pose as in Stargate SG1?",
+      choices: ["Ragnarok", "Hathor", "Yu", "Ra"],
+      answer: 3
+    },
+    {
+      question: "Who is the leader of the Tok'ra, the rebel faction of the Goa'uld who oppose the System Lords?",
+      choices: ["Jacob Carter / Selmak", "Jean-Luc Picard", "Johnny Rico", "Diz"],  
+      answer: 0
+    },
+    {
+      question: "What is the name of the planet that the SG1 team travels to in the episode 'Window of Opportunity,' where they become trapped in a time loop?",
+      choices: ["Pluto", "Andromeda", "P4X-639", "The Moon"],  
+      answer: 2
+    },
+    {
+      question: "What is the name of the planet where the SG1 team discovers the Lost City of the Ancients?",
+      choices: ["Mars", "Venus", "Earth", "Atlantis"],
+      answer: 3
+    },
+    {
+      question: "Who is the first main character to leave the SG1 team?",
+      choices: ["Data", "Jean Rasczak", "Logan", "Dr. Daniel Jackson"],
+      answer: 3
+    },
+    {
+      question: "What is the name of the Earth ship that serves as the primary mode of transportation for SG1?",
+      choices: ["USS Enterprise", "The Prometheus", "Vorlon Cruiser", "Thargoid Mothership"],
+      answer: 1
+    },
+    {
+      question: "What is the name of the device that allows SG1 to communicate across vast distances via a subspace field?",
+      choices: ["The Long-range communication device (LRC)", "Walkie Talkies", "DHD", "Short Range Thermometer"],
+      answer: 0
+    },
+    {
+      question: "What is the name of the SG1 team member who is an astrophysicist and serves as the main scientific advisor for the team?",
+      choices: ["Dr Raj Koothrapalli", "Dr Samantha Carter", "Dr Crusher", "Dr Bob"],
+      answer: 1
+    },
+    {
+      question: "What is the name of the planet where SG1 first encounters the Replicators?",
+      choices: ["P3X-489", "Coruscant", "Dagobah", "Hala"],
+      answer: 3
+    },
+    {
+      question: "What is the name of the Goa'uld who serves as the main villain during the final two seasons of SG1?",
+      choices: ["Zeus", "Seth", "Horus", "Ba'al"],
+      answer: "Ba'al"
+    },
+    {
+      question: "What is the name of the organization that oversees Stargate Command and the SG teams?",
+      choices: ["Minipax", "The FBI", "The IOA", "DoE"],
+      answer: 2
+    },
+    {
+      question: "What is the name of the device that allows SG1 to dial the Stargate to other planets?",
+      choices: ["The Dial Home Device (DHD)", "The Holodeck", "The Tardis", "The DeLorean"],
+      answer: 0
+    }
+]
